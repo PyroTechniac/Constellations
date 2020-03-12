f5ccd80a0f7b4a58445440ba3b7923be9821ad9f
Gryffon Bellish <owenbellish@gmail.com>
2020-03-12 12:36:15 -0400

    add base stopwatch

---
+++ src/lib/util/Stopwatch.ts
@@ -0,0 +1,53 @@
+import {performance} from 'perf_hooks';
+
+/**
+ * The stopwatch class, uses native node to get the most accurate timer.
+ */
+export class Stopwatch {
+	/**
+	 * The number of digits to appear after the decimal point when returning the friendly duration
+	 */
+	public digits: number;
+
+	/**
+	 * The start time of this stopwatch
+	 */
+	private _start: number = performance.now();
+
+	/**
+	 * The end time of this stopwatch
+	 */
+	private _end: number | null = null;
+
+	/**
+	 * Starts a new stopwatch
+	 * @param digits The amount of digits after the decimal point
+	 */
+	public constructor(digits = 2) {
+		this.digits = digits;
+	}
+
+	/**
+	 * The duration of this stopwatch since start or start to end if the stopwatch has stopped
+	 */
+	public get duration(): number {
+		return (this._end ?? performance.now()) - this._start;
+	}
+
+	/**
+	 * If the stopwatch is running or not
+	 */
+	public get running(): boolean {
+		return Boolean(!this._end);
+	}
+
+	/**
+	 * Restarts the stopwatch (Returns a running state).
+	 * @chainable
+	 */
+	public restart(): this {
+		this._start = performance.now();
+		this._end = null;
+		return this;
+	}
+}
